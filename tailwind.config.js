3b1d117568a45fe08cc5a4912eafa770c6347cb2
이승현 <poop0309>
2026-09-24 19:16:14 +0900

    feat: 라이트/다크 모드 테마 전환 기능 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // 이 설정이 꼭 있어야 합니다!
+  darkMode: 'class', // 이 부분이 반드시 있어야 합니다!
   theme: {
     extend: {},
   },
